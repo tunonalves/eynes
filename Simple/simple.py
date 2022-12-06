50bdd4a5163225a7b4f9350299c765f3347ec473
Federico Tuñon Alves <tunonalves@gmail.com>
2022-12-06 18:21:02 -0300

    simple

--- Simple/simple.py
+++ Simple/simple.py
@@ -1,2 +1,17 @@
 #Simple
+import random
+lista = []
+
+def age():
+    ages = random.randrange(1,100)
+    return ages
+
+lista = [
+    dict(
+        zip([x],[age()])
+        ) 
+        for x in range(0,10)
+    ]
+
+print(lista)
 
