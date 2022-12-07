ae714c7fb855672fd0b4da98d88da0c0517c9f76
Federico Tuñon Alves <tunonalves@gmail.com>
2022-12-06 23:42:29 -0300

    matriz

--- Matriz/Matriz.py
+++ Matriz/Matriz.py
@@ -1,11 +1,19 @@
 #Matriz
 import random
 
+def num_aleatorio():
+    num = random.randrange(1,10)
+    return num
 
-def listaAleatorios():
-    lista = [0]
+def matriz():
+    lista2 = []
     for i in range(5):
-        lista[i] = random.random()
-    return lista
+        lista2.append([])
+        for j in range(5):
+            lista2[i].append(num_aleatorio())	
+    matrix_length = len(lista2)
+    for i in range(matrix_length):
+        print(lista2[i])
+    return lista2
     
-print(listaAleatorios())
+matriz()
